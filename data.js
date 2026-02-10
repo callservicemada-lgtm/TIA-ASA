b49f6b02374d2aecbbc2c5ab5262d097f01adfe1
callservicemada-lgtm <callservicemada@gmail.com>
2026-02-10 13:26:12 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,78 @@
+/* TSY MAINTSY MISY NY "var ALL_JOBS =" EO AM-PIANDOHANA. */
+
+var ALL_JOBS = [
+    // --- IREO URGENT (MISY LOKO MENA SY BADGE "MAIKA") ---
+    {
+        "cat": "Urgent",
+        "title": "Mpiambina (Agent de Sécurité) - MAIKA",
+        "loc": "Antananarivo (Ivato)",
+        "img": "https://images.unsplash.com/photo-1582139329536-e7284fece509?w=400",
+        "tel": "0344521600",
+        "link": "",
+        "date": "12 Feb 2026",
+        "dossier": [
+            "Certificat de Résidence",
+            "Copie CIN Certifiée",
+            "Casier Judiciaire (Bulletin n°3)"
+        ]
+    },
+    {
+        "cat": "Urgent",
+        "title": "Gérant de Magasin - Urgent",
+        "loc": "Toamasina (Bazar Be)",
+        "img": "https://images.unsplash.com/photo-1534452203293-494d7ddbf7e0?w=400",
+        "tel": "0321122233",
+        "link": "https://facebook.com/job_urgent",
+        "date": "14 Feb 2026",
+        "dossier": [
+            "Diplôme BACC na mihoatra",
+            "Certificat de Résidence",
+            "Sary 4x4 isa 2"
+        ]
+    },
+
+    // --- IREO NORMAL (FISEHONY TSOTRA) ---
+    {
+        "cat": "Asa",
+        "title": "Vendeuse en Boutique",
+        "loc": "Antsirabe",
+        "img": "https://images.unsplash.com/photo-1441986300917-64674bd600d8?w=400",
+        "tel": "0335544433",
+        "link": "",
+        "date": "05 Mar 2026",
+        "dossier": [
+            "Certificat de Résidence",
+            "Diplôme BEPC",
+            "CV miloko"
+        ]
+    },
+    {
+        "cat": "BACC",
+        "title": "Mpitan-kaonty (Comptable Junior)",
+        "loc": "Mahajanga",
+        "img": "https://images.unsplash.com/photo-1554224155-6726b3ff858f?w=400",
+        "tel": "0340011122",
+        "link": "https://site.mg/job_bacc",
+        "date": "10 Mar 2026",
+        "dossier": [
+            "Diplôme BACC+2 amin'ny Fitantanana",
+            "Certificat de Résidence",
+            "Passport (raha misy)"
+        ]
+    },
+    {
+        "cat": "Concours",
+        "title": "Mpampianatra (ENS)",
+        "loc": "Manerana ny Nosy",
+        "img": "https://images.unsplash.com/photo-1524178232363-1fb2b075b655?w=400",
+        "tel": "0349988877",
+        "link": "https://education.gov.mg",
+        "date": "20 Mar 2026",
+        "dossier": [
+            "Diplôme BACC na Licence",
+            "Certificat de Résidence",
+            "Copie CIN",
+            "Casier Judiciaire"
+        ]
+    }
+];
---
+++ lien.js
@@ -0,0 +1,4 @@
+var ALL_LINKS = {
+    "hafatra": "Tonga soa eto amin'ny TIA-ASA! Manampy anao hahita asa malaky izahay.",
+    "finday": "034 45 216 00"
+};
